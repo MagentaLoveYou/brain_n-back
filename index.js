bc6506d5faa3ae835512178d7d0c5ca87d964611
MagentaLoveYou <95106610+MagentaLoveYou@users.noreply.github.com>
2023-02-16 00:49:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
         } else {
             clck--;
         };
-        if (repeat === false && repeat_repeat) {
+        if (repeat === false && repeat_repeat === true) {
           incorrect--;
           $("#score").text("Correct: " + correct + " Incorrect: " + incorrect);
         };
