14552571676c27805c2d4567c0964aad995c3ae5
MagentaLoveYou <95106610+MagentaLoveYou@users.noreply.github.com>
2023-02-16 10:43:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,13 @@
     var proverka = true;
 
     function displayRandomItem() {
-        if (previous[previous.length - (level + 2)] === previous[previous.length - 2] && repeat === true )  {
-          incorrect++;
-          $("#score").text("Correct: " + correct + " Incorrect: " + incorrect);
-          repeat_repeat = true;
-          proverka = false;
-        } else {
-            clck--;
+        //if (previous[previous.length - (level + 2)] === previous[previous.length - 2] && repeat === true )  {
+          //incorrect++;
+        //  $("#score").text("Correct: " + correct + " Incorrect: " + incorrect);
+          //repeat_repeat = true;
+         // proverka = false;
+       // } else {
+           // clck--;
         };    
         var percentage = [];
         flag = true; 
